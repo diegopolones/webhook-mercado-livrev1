8b0f0456b99337ffc1a7463f570e347a0ffd5b6a
diegopolones <polones.art.br@gmail.com>
2025-10-02 14:32:58 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+export const CONFIG = {
+  // Mercado Livre
+  ML_CLIENT_ID: '1856861988934927',
+  ML_CLIENT_SECRET: 'NstLBRNP9XEvp27J0wMclxxnsouL3oVb',
+  ML_REFRESH_TOKEN: 'TG-68cc1d23ddb77a0001e21b1b-73605926',
+  ML_USER_ID: '73605926',
+  
+  // Google Sheets
+  GOOGLE_SHEET_ID: '1OLsHJyDRl8G9Be_fEvv11LCCmOq5jz2-WqPzTVN0EN8', // ← Você vai me fornecer
+  
+  // Webhook
+  WEBHOOK_URL: 'https://webhook-mercado-livrev1.vercel.app/api/webhook'
+};
