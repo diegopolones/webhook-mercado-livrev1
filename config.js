268761aef16ed60d2a5d6f9e284b3b2456fcc9b1
diegopolones <polones.art.br@gmail.com>
2025-10-02 14:38:20 -0300

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,11 @@ export const CONFIG = {
   ML_USER_ID: '73605926',
   
   // Google Sheets
-  GOOGLE_SHEET_ID: '1OLsHJyDRl8G9Be_fEvv11LCCmOq5jz2-WqPzTVN0EN8', // ← Você vai me fornecer
+  GOOGLE_SHEET_ID: '1OLsHJyDRl8G9Be_fEvv11LCCmOq5jz2-WqPzTVN0EN8',
+  
+  // Service Account (você vai preencher depois)
+  GOOGLE_SERVICE_ACCOUNT_EMAIL: 'seu-email-da-service-account@seu-projeto.iam.gserviceaccount.com',
+  GOOGLE_PRIVATE_KEY: '-----BEGIN PRIVATE KEY-----\nSUA_CHAVE_PRIVADA_AQUI\n-----END PRIVATE KEY-----\n',
   
   // Webhook
   WEBHOOK_URL: 'https://webhook-mercado-livrev1.vercel.app/api/webhook'
